9be804baf0735dcf689520caf5e2804fbb007481
LetsMelon <domi.m@outlook.com>
2022-05-29 14:44:28 +0200

    implement workspace for tags lists

--- anonymizer/src/meta.rs
+++ anonymizer/src/meta.rs
@@ -1,6 +1,7 @@
 use dicom_core::Tag;
 use dicom_core::value::DicomDateTime;
 use derive_more::{Display};
+use tags_list::List as TagsList;
 
 #[derive(Display, Clone, Debug)]
 pub enum PatientSex {
@@ -38,6 +39,30 @@ pub struct AnonymizerMeta {
     pub(crate) patient_sex: Option<PatientSex>,
 }
 
+pub enum RemoveTagsInput {
+    Vec(Vec<Tag>),
+    List(TagsList),
+    VecList(Vec<TagsList>)
+}
+
+impl From<Vec<Tag>> for RemoveTagsInput {
+    fn from(v: Vec<Tag>) -> Self {
+        RemoveTagsInput::Vec(v)
+    }
+}
+
+impl From<TagsList> for RemoveTagsInput {
+    fn from(t: TagsList) -> Self {
+        RemoveTagsInput::List(t)
+    }
+}
+
+impl From<Vec<TagsList>> for RemoveTagsInput {
+    fn from(vt: Vec<TagsList>) -> Self {
+        RemoveTagsInput::VecList(vt)
+    }
+}
+
 impl AnonymizerMetaBuilder {
     pub fn remove_tag(&mut self, value: Tag) -> &mut Self {
         let mut obj = self;
@@ -50,11 +75,23 @@ impl AnonymizerMetaBuilder {
         obj
     }
 
-    pub fn remove_tags(&mut self, values: Vec<Tag>) -> &mut Self {
+    pub fn remove_tags(&mut self, values: RemoveTagsInput) -> &mut Self {
         let obj = self;
 
-        for item in values {
-            obj.remove_tag(item);
+        match values {
+            RemoveTagsInput::Vec(v) => {
+                for item in v {
+                    obj.remove_tag(item);
+                }
+            }
+            RemoveTagsInput::List(v) => {
+                obj.remove_tags(v.value().into());
+            },
+            RemoveTagsInput::VecList(vt) => {
+                for item in vt {
+                    obj.remove_tags(item.into());
+                }
+            }
         }
 
         obj
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,6 +2,7 @@ use dicom_core::chrono::FixedOffset;
 use dicom_core::Tag;
 use dicom_core::value::{DicomDate, DicomDateTime};
 use anonymizer::{Anonymizer, PatientSex};
+use tags_list::List;
 
 fn main() {
     let mut t2 = Anonymizer::from_file("/Users/letsmelon/Desktop/test.dcm").unwrap();
@@ -12,19 +13,12 @@ fn main() {
                 DicomDate::from_ymd(2022, 1, 1).unwrap(),
                 FixedOffset::east(0))
             )
-            .remove_tag(Tag {
-                0: 0x0010,
-                1: 0x0040,
-            })
-            .remove_tags(vec![Tag {
-                0: 0x0010,
-                1: 0x0020,
-            }])
+            .remove_tags(List::PATIENT.into())
             .patient_sex(PatientSex::O)
             .build().unwrap()
     );
 
     t2.anonymize();
 
-    t2.save("/Users/letsmelon/Desktop/test_out_3.dcm");
+    // t2.save("/Users/letsmelon/Desktop/test_out_3.dcm");
 }
