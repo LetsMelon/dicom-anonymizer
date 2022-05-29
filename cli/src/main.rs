8526e0b4e7ef445d92ba38d950eefe77dbf6efd7
LetsMelon <domi.m@outlook.com>
2022-05-29 21:50:03 +0200

    implement cli for anonymizer

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,24 +1,85 @@
+use std::u16;
 use dicom_core::chrono::FixedOffset;
 use dicom_core::Tag;
-use dicom_core::value::{DicomDate, DicomDateTime};
+use dicom_core::value::DicomDateTime;
 use anonymizer::{Anonymizer, PatientSex};
-use tags_list::List;
+use anyhow::Result;
+use clap::Parser;
+use std::str::FromStr;
+use chrono::{DateTime, NaiveDate, NaiveTime, Utc};
 
-fn main() {
-    let mut t2 = Anonymizer::from_file("/Users/letsmelon/Desktop/test.dcm").unwrap();
-    t2.meta(
-        Anonymizer::meta_builder()
-            .patient_name("Domenic Melcher")
-            .patient_birth_date(DicomDateTime::from_date(
-                DicomDate::from_ymd(2022, 1, 1).unwrap(),
-                FixedOffset::east(0))
-            )
-            .remove_tags(List::PATIENT.into())
-            .patient_sex(PatientSex::O)
-            .build().unwrap()
-    );
+#[derive(Parser, Debug)]
+#[clap(author="Domenic Melcher", version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long, parse(from_os_str))]
+    input: std::path::PathBuf,
 
-    t2.anonymize();
+    #[clap(short, long, parse(from_os_str))]
+    output: std::path::PathBuf,
 
-    // t2.save("/Users/letsmelon/Desktop/test_out_3.dcm");
+    #[clap(short, long, multiple_values = true)]
+    patient_name: Option<Vec<String>>,
+
+    #[clap(long)]
+    patient_sex: Option<String>,
+
+    #[clap(long)]
+    patient_birth_date: Option<String>,
+
+    #[clap(short, long, multiple_values = true, value_delimiter = ',')]
+    remove_tags: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let args: Cli = Cli::parse();
+
+    // println!("{:?}", args);
+
+    let mut obj = Anonymizer::from_file(&args.input.to_string_lossy())?;
+    let mut builder = Anonymizer::meta_builder();
+
+    match args.patient_name {
+        Some(v) => {
+            builder.patient_name(v.join(" "));
+        }
+        None => (),
+    }
+
+    match args.patient_sex {
+        Some(ps) => {
+            builder.patient_sex(PatientSex::from_str(&ps)?);
+        },
+        None => (),
+    };
+
+    match args.patient_birth_date {
+        Some(pbd) => {
+            let ndt = NaiveDate::parse_from_str(&*pbd, "%Y-%m-%d")?
+                .and_time(NaiveTime::from_hms(0, 0,0));
+            let dt_offset: DateTime<FixedOffset> = DateTime::<Utc>::from_utc(ndt, Utc).into();
+            builder.patient_birth_date(DicomDateTime::try_from(&dt_offset)?);
+        },
+        None => (),
+    };
+
+    let mut remove_tags = Vec::<Tag>::new();
+    for item in args.remove_tags {
+        let splitted = item.split("-").collect::<Vec<&str>>();
+
+        let group_number = u16::from_str_radix( splitted[0].trim_start_matches("0x"), 16)?;
+        let element_number = u16::from_str_radix( splitted[1].trim_start_matches("0x"), 16)?;
+        remove_tags.push(Tag {
+            0: group_number,
+            1: element_number,
+        });
+    }
+    builder.remove_tags(remove_tags.into());
+
+    obj.meta(builder.build()?);
+
+    obj.anonymize();
+
+    obj.save(&args.output.to_string_lossy())?;
+
+    Ok(())
 }
