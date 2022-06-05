cc0e334f2464760b1864497b2a9e06c9a51dbee3
LetsMelon <domi.m@outlook.com>
2022-06-05 17:09:47 +0200

    implement own tag and remove wrapper over dicom_core::Tag

--- anonymizer_lib/src/tag.rs
+++ anonymizer_lib/src/tag.rs
@@ -1,33 +1,27 @@
+use dicom_core::header::{ElementNumber, GroupNumber};
 use dicom_core::Tag;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Debug, Clone)]
-pub struct CustomTag(Tag);
-
-impl Serialize for CustomTag {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer {
-        todo!()
-    }
-}
-
-impl<'de> Deserialize<'de> for CustomTag {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where D: Deserializer<'de> {
-        todo!()
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CustomTag {
+    group: GroupNumber,
+    element: ElementNumber,
 }
 
 impl From<Tag> for CustomTag {
     fn from(v: Tag) -> Self {
-        Self {
-            0: v
+        CustomTag {
+            group: v.0,
+            element: v.1
         }
     }
 }
 
 impl From<CustomTag> for Tag {
     fn from(ct: CustomTag) -> Self {
-        ct.0
+        Tag {
+            0: ct.group,
+            1: ct.element
+        }
     }
 }
